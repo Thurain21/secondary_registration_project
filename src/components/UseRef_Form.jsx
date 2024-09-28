73c9d53eae5653d189be8291230eb4bfe392ae4c
Thurain21 <thurainkosg@gmail.com>
2024-09-28 18:57:06 +0800

    add useref form

---
+++ src/components/UseRef_Form.jsx
@@ -0,0 +1,45 @@
+import React, { useRef } from 'react'
+import './UseRef_Form.css'
+
+function UseRef_Form() {
+
+    const name = useRef('');
+    const email = useRef('');
+    const region = useRef('');
+
+    const regions = ['Singapore','Japan','Korea','Thailand','China'];
+    
+    const onClickEvent = () => {
+        const data = {
+            name : name.current.value,
+            email : email.current.value,
+            region : region.current.value
+        }
+
+        console.log(data);
+        
+    }
+    return (
+
+    <div className="container">
+        <form>
+            <div>
+                <input type="text" ref={name} className="user_name" placeholder='Enter your name' />
+            </div>
+            <div>
+                <input type="email" ref={email} className="user_email" placeholder='Enter your name' />
+            </div>
+            <div>
+                <select className="user_region" ref={region}>
+                    {regions.map((item,index) => <option key={index} value={item}>{item}</option>)}
+                </select>
+            </div>
+            <div>
+                <input type="button" value="Register" onClick={()=>onClickEvent()} className="user_btn" />
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default UseRef_Form
