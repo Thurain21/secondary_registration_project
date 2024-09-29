1421a4c6882b9c21f349786c6b5b63f5b4ae4ac4
Thurain21 <thurainkosg@gmail.com>
2024-09-29 19:04:36 +0800

    create child jsx

---
+++ src/components/Child.jsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react'
+import ThemeContext from '../context/ThemeContext';
+
+
+function Child() {
+    const rootData = useContext(ThemeContext);
+  return (
+    <div>
+        <h1>Child</h1>
+        <h2>{rootData}</h2>
+    </div>
+  )
+}
+
+export default Child;
