ef2b9ba2583a2ef14d3454f2b260015368b8a1bf
Thurain21 <thurainkosg@gmail.com>
2024-10-02 18:11:32 +0800

    modified for api call

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 // eslint-disable-next-line
 import "./App.css";
-import ChildTwo from "./components/ChildTwo";
-import UseRef_Form from "./components/UseRef_Form";
-import UseState_Form from "./components/UseState_Form";
-import SampleContextTwo from "./context/SampleContextTwo";
+import SampleAPI from "./components/SampleAPI";
 function App() {
   const data = 'This is data form root.';
   return (
     <>
-      <div className="App">
-        
-        {/* eslint-disable-next-line react/jsx-pascal-case */}
-        <UseState_Form />
-      </div>
-      <div>
-        {/* eslint-disable-next-line react/jsx-pascal-case */}
-        <UseRef_Form />
-      </div>
-      <div>
-        <SampleContextTwo.Provider value={data}>
-          <ChildTwo/>
-        </SampleContextTwo.Provider>
-      </div>
+      <SampleAPI/>
     </>
   );
 }
