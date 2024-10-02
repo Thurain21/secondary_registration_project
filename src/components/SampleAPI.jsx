cb8107f54a9133e8eb4aeda2533b451a7131dd0b
Thurain21 <thurainkosg@gmail.com>
2024-10-02 18:13:13 +0800

    add api call jsx

---
+++ src/components/SampleAPI.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+
+function SampleAPI() {
+  const [products, setProducts] = useState([]);
+
+  const fetchData = async () => {
+    const data = await fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .catch((error) => console.log(error.message));
+    setProducts(data);
+    console.log(products);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <h1>All Products</h1>
+      <div>
+        {products.map((product) => (
+          <li key={product.title}>{product.title}</li>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default SampleAPI;
